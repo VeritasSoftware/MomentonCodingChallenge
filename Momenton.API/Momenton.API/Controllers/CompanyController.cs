6a1f465860b9b791086c38df86c21c1f0db33dfb
VeritasSoftware <shantanu_negi@hotmail.com>
2018-05-11 23:03:18 +1000

    added comments to controller.

--- Momenton.API/Momenton.API/Controllers/CompanyController.cs
+++ Momenton.API/Momenton.API/Controllers/CompanyController.cs
@@ -5,17 +5,28 @@ using System;
 
 namespace Momenton.API.Controllers
 {
+    /// <summary>
+    /// Company controller
+    /// </summary>
     [Produces("application/json")]
     [Route("api/Company")]
     public class CompanyController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="employeeRepository">The injected employee repository</param>
         public CompanyController(IEmployeeRepository employeeRepository)
         {
             _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
         }
 
+        /// <summary>
+        /// Get company heirarchy
+        /// </summary>
+        /// <returns><see cref="EmployeeManager"/></returns>
         [HttpGet("hierarchy")]
         public EmployeeManager GetCompanyHierarchy()
         {
