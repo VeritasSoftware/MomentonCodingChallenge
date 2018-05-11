8d572097f067f0dbc1e7dc59cfbc257512f3a27a
VeritasSoftware <shantanu_negi@hotmail.com>
2018-05-11 22:07:39 +1000

    added comany controller and swagger ui.

---
+++ Momenton.API/Momenton.API/Controllers/CompanyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Momenton.Repository;
+using Momenton.Repository.Entity;
+using System;
+
+namespace Momenton.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Company")]
+    public class CompanyController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public CompanyController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+        }
+
+        [HttpGet("hierarchy")]
+        public EmployeeManager GetCompanyHierarchy()
+        {
+            return _employeeRepository.GetCompanyHierarchy();
+        }
+    }
+}
